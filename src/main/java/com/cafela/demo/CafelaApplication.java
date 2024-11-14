39be0bb89888e5665236648814c13e0515de95f8
Aaron 'Da Possession Ends Here' Zhang <aarondzhang33@gmail.com>
2024-11-13 21:14:56 -0800

    initial commit

---
+++ src/main/java/com/cafela/demo/CafelaApplication.java
@@ -0,0 +1,13 @@
+package com.cafela.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafelaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cafela/demo/CafelaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafela.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafelaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
